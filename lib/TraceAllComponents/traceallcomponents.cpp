7b87e68759bdf855fb7e901787aa68ed60eb09f4
lcy8047 <lcy8047@gmail.com>
2022-04-03 18:56:47 -0400

    Add PrintByFunction

--- lib/TraceAllComponents/traceallcomponents.cpp
+++ lib/TraceAllComponents/traceallcomponents.cpp
@@ -39,12 +39,11 @@ void PrintAllComponent( SVF::SVFModule *svfModule )
                 outs() << pasMsg( "<" ) << util->inst2idx( inst ) << pasMsg( util->inst2str( inst ) ) << pasMsg( " >" )
                        << pasMsg( " : " ) << inst << "\n";
                 outs() << pasMsg( "TYPE : " ) << pasMsg( util->getOperatorTypeName( inst ) ) << "\n";
-                unsigned int opcode = inst->getOpcode();
 
                 for ( int i = 0; i < inst->getNumOperands(); i ++ ) {
                     llvm::Value *operand = inst->getOperand( i );
                     std::string operandName = operand->getName().str();
-                    Type::TypeID tid = operand->getType()->getTypeID();
+                    //Type::TypeID tid = operand->getType()->getTypeID();
                     outs() << pasMsg( "OPERAND : " ) << pasMsg( operandName ) << "\t\t";
 
                     unsigned int valueID = operand->getValueID();
@@ -62,6 +61,57 @@ void PrintAllComponent( SVF::SVFModule *svfModule )
                         
                     outs() << " : " << operand << "\n";
                 }
+            }
+        }
+    }
+}
+
+void PrintByFunction( llvm::Function *func )
+{
+    UtilDef *util = new UtilDef;
+
+    outs() << pasMsg( "Function : " + func->getName().str() ) << "\n";
+    outs() << pasMsg( "ARG : ");
+    for ( auto it = func->arg_begin(); it != func->arg_end(); it ++ ) {
+        outs() << pasMsg( (*it).getName().str() ) << ", ";
+    }
+    outs() << "\n";
+
+    util->instIdxer( func );
+
+    // Iterate basic blocks in function
+    for ( auto &basicBlock : *func ) {
+        outs() << "  " << pasMsg( "Basic Blocks : " ) << "\n"
+                << "  " << pasMsg( basicBlock.getName().str() ) << "\n";
+
+        // Iterate instructions in basic block
+        for ( auto &instruction : basicBlock ) {
+            Instruction *inst = &instruction;
+            outs() << pasMsg( "<" ) << util->inst2idx( inst ) << pasMsg( util->inst2str( inst ) ) << pasMsg( " >" )
+                    << pasMsg( " : " ) << inst << "\n";
+            outs() << pasMsg( "TYPE : " ) << pasMsg( util->getOperatorTypeName( inst ) ) << " : " << inst->getOpcode() << "\n";
+            //unsigned int opcode = inst->getOpcode();
+
+            for ( int i = 0; i < inst->getNumOperands(); i ++ ) {
+                llvm::Value *operand = inst->getOperand( i );
+                std::string operandName = operand->getName().str();
+                //Type::TypeID tid = operand->getType()->getTypeID();
+                outs() << pasMsg( "OPERAND : " ) << pasMsg( operandName ) << "\t\t";
+
+                unsigned int valueID = operand->getValueID();
+                if ( valueID == llvm::Value::ConstantIntVal ) {
+                    llvm::ConstantInt *constint = dyn_cast<ConstantInt>( operand );
+                    outs() << pasMsg( "Int Val! : " ) << pasMsg( std::to_string( constint->getSExtValue() ) );
+                }
+                else {
+                    outs() << pasMsg( "ValueID : " ) << pasMsg( std::to_string( operand->getValueID() ) );                               
+                }
+                outs() << pasMsg( "  type : ") << operand->getType()->getTypeID();
+                if ( operand->getType()->getTypeID() == Type::PointerTyID ) {
+                    outs() << " " << operand->getType()->getContainedType( 0 )->getTypeID();
+                }
+                    
+                outs() << " : " << operand << "\n";
             }
         }
     }
