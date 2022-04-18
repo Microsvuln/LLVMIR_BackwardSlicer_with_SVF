5b06559167ba3e4573a44e897e8bd36db33ab227
lcy8047 <lcy8047@gmail.com>
2022-04-18 18:22:41 -0400

    Update for global variable

--- lib/TraceAllComponents/traceallcomponents.cpp
+++ lib/TraceAllComponents/traceallcomponents.cpp
@@ -2,12 +2,52 @@
 #include "SVF-FE/LLVMUtil.h"
 #include "UtilDef.h"
 
+#include <llvm/IR/Attributes.h>
+
 #include <iostream>
 #include <vector>
 #include <string>
 
 using namespace SVF;
 using SVF::SVFUtil::pasMsg;
+
+/*
+void PrintOperands( Value *value )
+{
+    uint32_t valueid = value->getValueID();
+    Instruction *inst;
+    ConstantExpr *constexprval;
+
+    if ( Value::InstructionVal + Instruction::TermOpsBegin <= valueid && valueid <= Value::InstructionVal + Instruction::OtherOpsEnd ) {
+        inst = dyn_cast<Instruction>( value );
+    }
+    if ( valueid == Value::ConstantExprVal ) {
+        ConstantExpr *
+    }
+
+    for ( int i = 0; i < inst->getNumOperands(); i ++ ) {
+        llvm::Value *operand = inst->getOperand( i );
+        std::string operandName = operand->getName().str();
+        //Type::TypeID tid = operand->getType()->getTypeID();
+        outs() << pasMsg( "OPERAND : " ) << pasMsg( operandName ) << "\t\t";
+
+        unsigned int valueID = operand->getValueID();
+        if ( valueID == llvm::Value::ConstantIntVal ) {
+            llvm::ConstantInt *constint = dyn_cast<ConstantInt>( operand );
+            outs() << pasMsg( "Int Val! : " ) << pasMsg( std::to_string( constint->getSExtValue() ) );
+        }
+        else {
+            outs() << pasMsg( "ValueID : " ) << pasMsg( std::to_string( operand->getValueID() ) );                               
+        }
+        outs() << pasMsg( "  type : ") << operand->getType()->getTypeID();
+        if ( operand->getType()->getTypeID() == Type::PointerTyID ) {
+            outs() << " " << operand->getType()->getContainedType( 0 )->getTypeID();
+        }
+            
+        outs() << " : " << operand << "\n";
+    }
+}
+*/
 
 void PrintAllComponent( SVF::SVFModule *svfModule )
 {
@@ -103,6 +143,11 @@ void PrintByFunction( llvm::Function *func )
                     llvm::ConstantInt *constint = dyn_cast<ConstantInt>( operand );
                     outs() << pasMsg( "Int Val! : " ) << pasMsg( std::to_string( constint->getSExtValue() ) );
                 }
+                else if ( valueID == llvm::Value::GlobalVariableVal ) {
+                    llvm::GlobalVariable *global = dyn_cast<GlobalVariable>( operand );
+                    outs() << pasMsg( "Global Val! : " );
+                    global->print( outs() );
+                }
                 else {
                     outs() << pasMsg( "ValueID : " ) << pasMsg( std::to_string( operand->getValueID() ) );                               
                 }
@@ -114,9 +159,70 @@ void PrintByFunction( llvm::Function *func )
                 outs() << " : " << operand << "\n";
             }
             if ( inst->getOpcode() == Instruction::Call ) {
-                outs() << "Param Attr : ";
                 CallInst *ci = dyn_cast<CallInst>( inst );
-                Function *calledFunc = ci->getCalledFunction();
+                Value *called_op = ci->getCalledOperand();
+                Function *calledFunc;
+                if ( called_op->getValueID() == Value::FunctionVal ) {
+                    calledFunc = dyn_cast<Function>( called_op );
+                }
+                else if ( called_op->getValueID() == Value::ConstantExprVal ) {
+                    outs() << "constexpr call : ";
+                    called_op->print( outs() );
+                    outs() << "\n";
+
+                    ConstantExpr *ce_op = dyn_cast<ConstantExpr>( called_op );
+                    
+                    for ( int i = 0; i < ce_op->getNumOperands(); i ++ ) {
+                        Value *operand = ce_op->getOperand( i );
+                        std::string operandName = operand->getName().str();
+                        //Type::TypeID tid = operand->getType()->getTypeID();
+                        outs() << pasMsg( "  OPERAND : " ) << pasMsg( operandName ) << "\t\t";
+
+                        unsigned int valueID = operand->getValueID();
+                        if ( valueID == llvm::Value::ConstantIntVal ) {
+                            llvm::ConstantInt *constint = dyn_cast<ConstantInt>( operand );
+                            outs() << pasMsg( "Int Val! : " ) << pasMsg( std::to_string( constint->getSExtValue() ) );
+                        }
+                        else {
+                            outs() << pasMsg( "ValueID : " ) << pasMsg( std::to_string( operand->getValueID() ) );                               
+                        }
+                        outs() << pasMsg( "  type : ") << operand->getType()->getTypeID();
+                        if ( operand->getType()->getTypeID() == Type::PointerTyID ) {
+                            outs() << " " << operand->getType()->getContainedType( 0 )->getTypeID();
+                        }
+                            
+                        outs() << " : " << operand << "\n";
+                    }
+                    continue;
+                }
+                else {
+                    outs() << called_op->getValueID() << " : " << called_op->getValueName() << " indirect call\n";
+                    continue;
+                }
+
+                /*
+                inst->getMetadata();
+                Value::
+                
+               
+                */
+                //outs() << "Func type : ";
+                //outs() << calledFunc->getReturnType()
+                if ( calledFunc->getReturnType()->isVoidTy() ) {
+                    outs() << "void function\n";
+                }
+                else {
+                    outs() << "Not void function\n";
+                }
+
+                outs() << "param, arg : ";
+                for ( int i = 0; i < ci->getNumArgOperands(); i ++ ) {
+                    Value *param = ci->getArgOperand( i );
+                    Value *arg = calledFunc->getArg( i );
+                    outs() << param->getName() << ", " << arg->getName() << " | ";
+                }
+
+                outs() << "\nParam Attr : ";
                 for ( int i = 0; i < ci->getNumOperands() - 1; i ++ ) {
                     //Attribute::hasAttribute(StringRef Kind) 
                     Attribute attr = calledFunc->getArg( i )->getAttribute(Attribute::AttrKind::Dereferenceable);
